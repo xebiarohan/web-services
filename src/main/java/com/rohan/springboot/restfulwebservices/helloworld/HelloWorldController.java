41b6c86255207d5ac219e82d507211d67a2d7670
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2023-05-11 19:17:10 +0400

    Adding internationalization

--- src/main/java/com/rohan/springboot/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rohan/springboot/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,15 +1,32 @@
 package com.rohan.springboot.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String hello() {
         return "Hello world";
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource
+                .getMessage("good.morning.message", null,"Default message", locale);
     }
 
     @GetMapping("/hello-world-bean")
