e0ce6fbfcbbd7c0c29aa558cb2b05ede6bfc42d9
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2023-05-12 12:50:48 +0400

    Adding other ways of versioning- header, media_type and param

--- src/main/java/com/rohan/springboot/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/rohan/springboot/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,33 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPerson() {
         return new PersonV2(new Name("Bob", "charlie"));
     }
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonUsingParams() {
+        return new PersonV1("Bob charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonUsingParams() {
+        return new PersonV2(new Name("Bob", "charlie"));
+    }
+
+    @GetMapping(path = "/person", headers = "X_API_VERSION=1")
+    public PersonV1 getFirstVersionOfPersonUsingHeaders() {
+        return new PersonV1("Bob charlie");
+    }
+
+    @GetMapping(path = "/person", headers = "X_API_VERSION=2")
+    public PersonV2 getSecondVersionOfPersonUsingHeaders() {
+        return new PersonV2(new Name("Bob", "charlie"));
+    }
+
+    @GetMapping(path = "/person", produces = "application/app-v1+json")
+    public PersonV1 getFirstVersionOfPersonUsingMediaType() {
+        return new PersonV1("Bob charlie");
+    }
+
+    @GetMapping(path = "/person", produces = "application/app-v2+json")
+    public PersonV2 getSecondVersionOfPersonUsingMediaType() { return new PersonV2(new Name("Bob", "charlie")); }
+
 }
