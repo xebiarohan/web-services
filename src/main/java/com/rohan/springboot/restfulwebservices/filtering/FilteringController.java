f637e22fc728709fa93d177af59118af0847348c
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2023-05-14 12:51:48 +0400

    static filtering

---
+++ src/main/java/com/rohan/springboot/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,13 @@
+package com.rohan.springboot.restfulwebservices.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public SomeBean filtering() {
+        return new SomeBean("value1", "value2", "value3");
+    }
+}
--- src/main/java/com/rohan/springboot/restfulwebservices/user/User.java
+++ src/main/java/com/rohan/springboot/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.rohan.springboot.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,9 +9,11 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have atleast 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth date must be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
