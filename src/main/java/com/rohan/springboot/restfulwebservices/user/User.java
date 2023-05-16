0a7ae586d216635079bb38dd313ee5a80bcff053
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2023-05-16 18:22:52 +0400

    Adding H2 database compatibility

--- src/main/java/com/rohan/springboot/restfulwebservices/user/User.java
+++ src/main/java/com/rohan/springboot/restfulwebservices/user/User.java
@@ -1,13 +1,23 @@
 package com.rohan.springboot.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+
+    @Id
+    @GeneratedValue()
     private Integer id;
+
+    @Column
     @Size(min = 2, message = "Name should have atleast 2 characters")
     @JsonProperty("user_name")
     private String name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user_details(id,birth_date,name) values (10001, current_date(), 'rohan')
+
+insert into user_details(id,birth_date,name) values (10002, current_date(), 'riddhi')
+
+insert into user_details(id,birth_date,name) values (10003, current_date(), 'a')
+
+insert into user_details(id,birth_date,name) values (10004, current_date(), 'b')
+
+insert into user_details(id,birth_date,name) values (10005, current_date(), 'c')
